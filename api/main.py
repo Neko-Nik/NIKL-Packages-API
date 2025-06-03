11da1afaf4c3900c1bde8e40035c944e53eb2a04
Neko Nik <nikhil@nekonik.com>
2025-06-03 13:47:07 +0530

    Update CORS middleware: add localhost:5173 to allowed origins

--- api/main.py
+++ api/main.py
@@ -30,7 +30,7 @@ app = FastAPI(
 # Add CROCS middle ware to allow cross origin requests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://nikl-pkg.nekonik.com", "http://localhost:3000", "https://api.nikl-pkg.nekonik.com"],
+    allow_origins=["https://nikl-pkg.nekonik.com", "http://localhost:3000", "https://api.nikl-pkg.nekonik.com", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
